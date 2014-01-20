2bcf69509d7e11f85fd1b4a2db3d16930c03f82e
TOM-TRAN <tt7@ualberta.ca>
2014-01-20 08:17:16 -0700

    Assignment 1 - First commit

--- README.md
+++ README.md
@@ -10,15 +10,32 @@ Make a simple webserver.
 Contributors / Licensing
 ========================
 
-Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
+Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle and Tom Tran.
 
 server.py contains contributions from:
 
 * Abram Hindle
 * Eddie Antonio Santos
+* Tom Tran
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
+Resources
+========================
+* Example of using only Python Sockets to return a HTML page
+	http://stackoverflow.com/questions/10114224/how-to-properly-send-http-response-with-python-using-socket-library-only
+
+* How a server Response Message is formatted:
+	http://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol#Response_message
+
+* Reading a whole file as one line:
+	http://ubuntuforums.org/showthread.php?t=443740
+
+* Commenting can cause indentation errors:
+	http://stackoverflow.com/questions/13701509/python-unexpected-indent-but-indentation-is-seemingly-correct-and-consistent
+
+
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import SocketServer
 # coding: utf-8
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Tom Tran
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,11 +28,74 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
+        
+        # Define available URIs
+        base_dir = '/'
+        base_dir2 = '/index.html'
+        deep_dir = '/deep'
+        deep_dir2 = '/deep/'
+        deep_dir3 = '/deep/index.html'
+        base_css = '/base.css'
+        deep_css = '/deep/deep.css'
+        deep_css2 = '/deep.css'
+
+        # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+
+
+        # Find the GET directory and set variables accordingly
+        self.directory = self.data.split()[1]
+
+        if self.directory == base_dir or self.directory == base_dir2:
+            self.response_file = 'www/index.html'
+            self.mimetype = 'text/html;charset=utf-8'
+        elif self.directory == deep_dir or self.directory == deep_dir2 or self.directory == deep_dir3:
+            self.response_file = 'www/deep/index.html'
+            self.mimetype = 'text/html;charset=utf-8'
+        elif self.directory == base_css:
+            self.response_file = 'www/base.css'
+            self.mimetype = 'text/css'
+        elif self.directory == deep_css or self.directory == deep_css2:
+            self.response_file = 'www/deep/deep.css'
+            self.mimetype = 'text/css'
+        else:
+            # 404 Not Found
+            self.response_proto = 'HTTP/1.1'
+            self.response_status = '404'
+            self.response_status_text = 'Not Found'
+            self.request.send('%s %s %s \n' % (self.response_proto, self.response_status, self.response_status_text))
+            self.response_body = '<html><h1><body>404 Not Found</body></h1><html>'
+            self.response_headers = "'Content-Type': 'text/html; encoding=utf8'\n'Content-Length': %s\n'Connection': 'close'\n" % (len(self.response_body))
+            self.request.send(self.response_headers)
+            self.request.send("\n")
+            self.request.send(self.response_body)
+            return
+
+
+        # Prepare the response header                    
+        self.response_proto = 'HTTP/1.1'
+        self.response_status = '200'
+        self.response_status_text = 'OK'
+        self.request.send('%s %s %s \n' % (self.response_proto, self.response_status, self.response_status_text))
+
+        
+        # Prepare to send the response file
+        self.file_pointer = open(self.response_file)
+               
+        try:
+            self.response_body = self.file_pointer.read()
+            self.response_headers = "Content-Type: %s\nContent-Length: %s\n" % (self.mimetype, len(self.response_body))
+            self.request.send(self.response_headers)
+            self.request.send("\n")
+            self.request.send(self.response_body)
+
+        finally:
+            self.file_pointer.close()
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
